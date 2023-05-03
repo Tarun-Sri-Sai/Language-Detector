3c15d8c6687f20ee51ae78417ada515a8b3e60bb
Tarun <tarunsrisaiv03@gmail.com>
2023-05-03 07:02:40 +0530

    Remove comments, Relocate benchmarks, Speedup code

--- ngram_char_lang.py
+++ ngram_char_lang.py
@@ -3,7 +3,7 @@ from json import load, dump
 from pandas import read_csv
 from string import punctuation
 from re import sub
-# from time import perf_counter
+from time import perf_counter
 from math import log
 
 
@@ -27,30 +27,22 @@ def get_ngrams(text, n):
 
 
 def compute_log_probs(ngrams):
-    result = {}
     sum_value = sum(value for _, value in ngrams.items())
-    for key, value in ngrams.items():
-        result[key] = log(value) - log(sum_value)
-
-    return result
+    return {key: log(value) - log(sum_value) for key, value in ngrams.items()}
 
 
 def get_log_prob(text_ngrams, lang_ngrams_log_probs):
-    result = 0
-    for ngram in text_ngrams:
-        result += lang_ngrams_log_probs.get(ngram, log(1e-10))
-
-    return result
+    return sum(lang_ngrams_log_probs.get(ngram, log(1e-10)) for ngram in text_ngrams)
 
 
 def main():
+    start_time = perf_counter()
     N = 3
     MAX_INPUT_CHARS = 1024
     cache_path = "json/cache.json"
     if not path.isfile(cache_path):
         system(f"echo {{}} > {cache_path}")
 
-    # start_time = perf_counter()
     csv_path = "csv/sentences.csv"
     if open(cache_path, "r", encoding="utf-8").read().strip() == "{}":
         langs_list, lang_ngrams_probs_list = get_langs_and_ngrams_log_probs(N, csv_path)
@@ -64,14 +56,12 @@ def main():
         langs_list = json_data["langs_list"]
         lang_ngrams_probs_list = json_data["lang_ngrams_probs_list"]
 
-    # end_time = perf_counter()
-    # total_probs = sum(sum(len(item) for item in item_list) for item_list in lang_ngrams_probs_list)
-    # print(f"Searched in {len(langs_list)} languages and " + f"{total_probs:,} probability distributions in {end_time - start_time:.3f}s\n")
     text = open("txt/input.txt", "r", encoding="utf-8").read().strip()[:MAX_INPUT_CHARS]
     text_ngrams = get_ngrams(text, N)
     log_prob_dist_list = [get_log_prob(text_ngrams, lang_ngrams_probs_list[i]) for i in range(len(langs_list))]
-    # print(*[f"{lang}: {prob}" for lang, prob in zip(langs_list, log_prob_dist_list)],  sep="\n", end="\n\n")
     print(langs_list[log_prob_dist_list.index(max(log_prob_dist_list))])
+    end_time = perf_counter()
+    print(f"\nProcess finished in {end_time - start_time:.2f}s")
 
 
 if __name__ == "__main__":

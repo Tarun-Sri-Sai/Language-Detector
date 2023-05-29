f62d80d3aa0cb0c8b8bb174812cc426ffed3d756
Tarun <tarunsrisaiv03@gmail.com>
2023-05-29 07:11:49 +0530

    Rename folders

--- src/ngram_char.py
+++ src/ngram_char.py
@@ -41,11 +41,11 @@ def predict(text):
     start_time = perf_counter()
     N = 3
     MAX_INPUT_CHARS = 1024
-    cache_path = "json/cache.json"
+    cache_path = path.join("cache", "cache.json")
     if not path.isfile(cache_path):
         system(f"echo {{}} > {cache_path}")
 
-    csv_path = "csv/sentences.csv"
+    csv_path = path.join("data", "sentences.csv")
     if open(cache_path, "r", encoding="utf-8").read().strip() == "{}":
         langs_list, lang_ngrams_log_probs_list = get_langs_and_ngrams_log_probs(
             N, csv_path)

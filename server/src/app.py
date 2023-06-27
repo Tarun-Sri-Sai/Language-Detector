89939b10458cd276aa2a23b080f9433794b6d80a
Tarun <tarunsrisaiv03@gmail.com>
2023-06-27 10:39:24 +0530

    Fix indents

--- server/src/app.py
+++ server/src/app.py
@@ -18,15 +18,17 @@ class App:
             os.system(f'echo {{}} > {self.cache_path}')
 
         if open(self.cache_path, 'r', encoding='utf-8').read().strip() == '{}':
-            self.langs_list, self.lang_ngrams_log_probs_list = self.get_langs_and_ngrams_log_probs(
+            (self.langs_list,
+             self.lang_ngrams_log_probs_list) = self.get_langs_and_ngrams_log_probs(
                 self.N, self.csv_path)
-            
+
             cache = {
                 'langs_list': self.langs_list,
                 'lang_ngrams_log_probs_list': self.lang_ngrams_log_probs_list
             }
             print(f'Writing cache to {self.cache_path}')
-            json.dump(cache, open(self.cache_path, 'w', encoding='utf-8'), indent=4)
+            json.dump(
+                cache, open(self.cache_path, 'w', encoding='utf-8'), indent=4)
         else:
             print(f'Reading cache from {self.cache_path}')
             json_data = json.load(open(self.cache_path, 'r', encoding='utf-8'))
@@ -39,10 +41,11 @@ class App:
 
         text_ngrams = self.get_ngrams(text, self.N)
 
-        log_prob_dist_list = [self.get_log_prob(text_ngrams, self.lang_ngrams_log_probs_list[i]) 
+        log_prob_dist_list = [self.get_log_prob(text_ngrams, self.lang_ngrams_log_probs_list[i])
                               for i in range(len(self.langs_list))]
-        
-        result = self.langs_list[log_prob_dist_list.index(max(log_prob_dist_list))]
+
+        result = self.langs_list[log_prob_dist_list.index(
+            max(log_prob_dist_list))]
         return result.upper()
 
     def get_ngrams(self, text, n):
@@ -56,19 +59,25 @@ class App:
 
     def compute_log_probs(self, ngrams):
         sum_value = sum(value for _, value in ngrams.items())
-        return {key: math.log(value) - math.log(sum_value) for key, value in ngrams.items()}
+        return {
+            key: math.log(value) - math.log(sum_value)
+            for key, value in ngrams.items()
+        }
 
     def get_langs_and_ngrams_log_probs(self, n, csv_path):
         train_csv = pd.read_csv(csv_path, encoding='utf-8').dropna()
         langs_list = train_csv['lang'].unique().tolist()
         lang_range = range(len(langs_list))
 
-        lang_text_list = [' '.join([sentence 
-                                          for sentence in train_csv[train_csv['lang'] == lang]['sentence']]) 
-                          for lang in langs_list]
+        lang_text_list = [' '.join(
+            [sentence for sentence in train_csv[train_csv['lang'] == lang]['sentence']])
+            for lang in langs_list]
         lang_ngrams_list = [self.get_ngrams(lang_text_list[i], n)
                             for i in lang_range]
-        return langs_list, [self.compute_log_probs(lang_ngrams_list[i]) for i in lang_range]
+        return langs_list, [self.compute_log_probs(lang_ngrams_list[i])
+                            for i in lang_range]
 
     def get_log_prob(self, text_ngrams, lang_ngrams_log_probs):
-        return sum(lang_ngrams_log_probs.get(ngram, math.log(1e-10)) for ngram in text_ngrams)
+        return sum(
+            lang_ngrams_log_probs.get(ngram, math.log(1e-10))
+            for ngram in text_ngrams)

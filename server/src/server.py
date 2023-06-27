125cc11cd1d8755b1e195dc8689180673d4e7d82
Tarun <tarunsrisaiv03@gmail.com>
2023-06-27 10:30:22 +0530

    Change app names

--- server/src/server.py
+++ server/src/server.py
@@ -1,18 +1,19 @@
 import flask
-import ngram_char as nc
+from app import App
 import flask_cors as fc
 
 
-app = flask.Flask(__name__)
-fc.CORS(app)
+flask_app = flask.Flask(__name__)
+fc.CORS(flask_app)
+lang_app = App()
 
 
-@app.route('/detect-language', methods=['GET'])
+@flask_app.route('/detect-language', methods=['GET'])
 def detect_language_endpoint():
     input_text = flask.request.args.get('input_text', '')
-    language_code = nc.detect_language(input_text)
+    language_code = lang_app.detect(input_text)
     return {'language_code': language_code}
 
 
 if __name__ == '__main__':
-    app.run()
+    flask_app.run()

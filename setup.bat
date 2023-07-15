86fab47157f0df374d32fa55f495d6c21aebd118
Tarun <tarunsrisaiv03@gmail.com>
2023-07-15 19:01:12 +0530

    Update pip command

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start cmd /k "cd server/src & pip install pandas & pip install flask & pip install -U flask-cors & exit"
+start cmd /k "pip install pandas flask flask-cors & exit"
 start cmd /k "cd language-detector & npm install & exit"

261b339a65eec27239076098eb5556470f3ad0c2
Tarun <tarunsrisaiv03@gmail.com>
2023-08-21 12:40:50 +0530

    Update batch files

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
 @echo off
 
 start cmd /k ^
-    "pip install pandas flask flask-cors" ^
-    "& exit"
+"pip install pandas flask flask-cors" ^
+"& exit"
+
 start cmd /k ^
-    "cd frontend" ^
-    "& npm install @angular/cli" ^
-    "& npm install" ^
-    "& npm audit fix" ^
-    "& exit"
+"cd frontend" ^
+"& npm install @angular/cli" ^
+"& npm install" ^
+"& npm audit fix" ^
+"& exit"

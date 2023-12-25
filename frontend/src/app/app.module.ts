91440e0d4a114c4571636fd0916789e437138c76
Tarun <tarunsrisaiv03@gmail.com>
2023-12-25 11:18:48 +0530

    Simplify directory structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { InputComponent } from './input/input.component';
-import { ResultComponent } from './result/result.component';
+import { TitleComponent } from './title.component';
+import { InputComponent } from './input.component';
+import { ResultComponent } from './result.component';
 
 @NgModule({
   declarations: [AppComponent, TitleComponent, InputComponent, ResultComponent],
